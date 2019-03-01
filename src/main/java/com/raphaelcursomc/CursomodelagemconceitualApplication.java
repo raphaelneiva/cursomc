c3b1a32aef5dc11db2e05a4e1736b0c3f15b0716
Raphael Neiva <raphaelpneiva@gmail.com>
2019-03-01 11:54:43 -0300

    Primeiro Commit - Projeto criado

---
+++ src/main/java/com/raphaelcursomc/CursomodelagemconceitualApplication.java
@@ -0,0 +1,13 @@
+package com.raphaelcursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomodelagemconceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomodelagemconceitualApplication.class, args);
+	}
+
+}
